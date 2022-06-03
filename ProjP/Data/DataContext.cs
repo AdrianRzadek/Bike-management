d202eac906f09c7a1c4c2f1fb2bfdaf780bbe7be
Adrian <mschxd@gmail.com>
2022-06-03 14:44:54 +0200

    DataContext

---
+++ ProjP/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjPIV.Data
+{
+    public class DataContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SQLEXPRESS;
+                                        Database = WypozyczalniaRowerow; Trusted_Connection = True; ");
+
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+
+        }
+    }
+}
