cdcbfa475e6b6baaa4f69d2c00b9b30fa7bc879f
Adrian <mschxd@gmail.com>
2022-06-06 17:18:31 +0200

    page 0.1

--- ProjP/MainWindow.xaml.cs
+++ ProjP/MainWindow.xaml.cs
@@ -29,13 +29,13 @@ namespace ProjP
         
         private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
         {
-            ButtonOpenMenu.Visibility = Visibility.Collapsed;
-            ButtonCloseMenu.Visibility = Visibility.Visible;
+           ButtonOpenMenu.Visibility = Visibility.Collapsed;
+           ButtonCloseMenu.Visibility = Visibility.Visible;
         }
 
         private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
         {
-            ButtonOpenMenu.Visibility = Visibility.Visible;
+           ButtonOpenMenu.Visibility = Visibility.Visible;
             ButtonCloseMenu.Visibility = Visibility.Collapsed;
         }
 
